e30100d95266a2f3b5f21e5a40e2a1de55286b4d
raiyannawaz <raiyannawaz95@gmail.com>
2025-03-25 22:22:13 +0530

    deployment changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, NavLink, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './Components/Navbar'
 import Login from './Pages/Login'
 import Dashboard from './Pages/Dashboard';
