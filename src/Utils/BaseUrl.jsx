8c1e07b2e0f5d9edcc05a435f95c7ad38656356e
raiyannawaz <raiyannawaz95@gmail.com>
2025-03-25 23:43:00 +0530

    minor changes

--- src/Utils/BaseUrl.jsx
+++ src/Utils/BaseUrl.jsx
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 const apiClient = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    timeout: 100000,
     headers: {
         'Content-Type' : 'application/json',
     }
